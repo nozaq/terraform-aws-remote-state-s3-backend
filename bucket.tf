52b04a3c070e7d76b045baf1bb88c1844c8066d9
nozaq <code@nozaq.com>
2020-08-10 16:54:01 +0900

    fix: remove region attribute (#24)
    Starting from AWS provider 3.x, region attribute becomes read-only.
    https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/version-3-upgrade#region-attribute-is-now-read-only

--- bucket.tf
+++ bucket.tf
@@ -139,7 +139,6 @@ resource "aws_s3_bucket" "replica" {
   provider = aws.replica
 
   bucket_prefix = var.replica_bucket_prefix
-  region        = data.aws_region.replica.name
   force_destroy = var.s3_bucket_force_destroy
 
   versioning {
