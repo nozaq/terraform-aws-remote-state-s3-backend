1f1ec6c0924e1d683f45974f0657ffa1007b384e
nozaq <code@nozaq.com>
2020-11-14 14:33:49 +0900

    fix: replace interporations with variable expressions

--- bucket.tf
+++ bucket.tf
@@ -140,7 +140,7 @@ data "aws_iam_policy_document" "state_force_ssl" {
     actions = ["s3:*"]
     effect  = "Deny"
     resources = [
-      "${aws_s3_bucket.state.arn}",
+      aws_s3_bucket.state.arn,
       "${aws_s3_bucket.state.arn}/*"
     ]
     condition {
@@ -161,7 +161,7 @@ data "aws_iam_policy_document" "replica_force_ssl" {
     actions = ["s3:*"]
     effect  = "Deny"
     resources = [
-      "${aws_s3_bucket.replica.arn}",
+      aws_s3_bucket.replica.arn,
       "${aws_s3_bucket.replica.arn}/*"
     ]
     condition {
