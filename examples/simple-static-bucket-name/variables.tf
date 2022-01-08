a278e20db7835532e6e3899c59f2dced8ced25cf
nozaq <code@nozaq.com>
2022-01-08 19:25:13 +0900

    refactor: do not hard-code auth method in examples

--- examples/simple-static-bucket-name/variables.tf
+++ examples/simple-static-bucket-name/variables.tf
@@ -1,9 +1,3 @@
-variable "access_key" {
-}
-
-variable "secret_key" {
-}
-
 variable "region" {
   description = "The AWS region in which resources are set up."
   default     = "us-east-1"
--- examples/simple/variables.tf
+++ examples/simple/variables.tf
@@ -1,9 +1,3 @@
-variable "access_key" {
-}
-
-variable "secret_key" {
-}
-
 variable "region" {
   description = "The AWS region in which resources are set up."
   default     = "us-east-1"
@@ -14,4 +8,3 @@ variable "replica_region" {
   description = "The AWS region to which the state bucket is replicated."
   default     = "us-west-1"
 }
-
