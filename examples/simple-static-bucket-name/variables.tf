e3b406e83c440d42e3bb5425b148a9cc7dbf0378
nozaq <code@nozaq.com>
2022-01-10 08:19:04 +0900

    refactor: add tflint checks
    * chore: use pre-commit hooks in CI workflow
    
    * refactor: define variable types

--- examples/simple-static-bucket-name/variables.tf
+++ examples/simple-static-bucket-name/variables.tf
@@ -1,11 +1,13 @@
 variable "region" {
   description = "The AWS region in which resources are set up."
+  type        = string
   default     = "us-east-1"
 }
 
 
 variable "replica_region" {
   description = "The AWS region to which the state bucket is replicated."
+  type        = string
   default     = "us-west-1"
 }
 
