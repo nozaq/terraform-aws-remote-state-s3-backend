ea6420526f843ca9ce0cea67a426e767d7ea7b00
nozaq <code@nozaq.com>
2022-10-22 10:55:11 +0900

    fix: do not allow uppercase letters for bucket names

--- variables.tf
+++ variables.tf
@@ -187,17 +187,17 @@ variable "s3_bucket_name" {
   type        = string
   default     = ""
   validation {
-    condition     = length(regexall("^[A-Za-z0-9][A-Za-z0-9\\-.]{1,61}[A-Za-z0-9]$", var.s3_bucket_name)) > 0
-    error_message = "Input variable s3_bucket_name is invalid. Please refer to https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html"
+    condition     = length(regexall("^[a-z0-9][a-z0-9\\-.]{1,61}[a-z0-9]$", var.s3_bucket_name)) > 0
+    error_message = "Input variable s3_bucket_name is invalid. Please refer to https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html."
   }
 }
 variable "s3_bucket_name_replica" {
   description = "If override_s3_bucket_name is true, use this bucket name for replica instead of dynamic name with bucket_prefix"
   type        = string
   default     = ""
-    validation {
-    condition     = length(regexall("^[A-Za-z0-9][A-Za-z0-9\\-.]{1,61}[A-Za-z0-9]$", var.s3_bucket_name_replica)) > 0
-    error_message = "Input variable s3_bucket_name_replica is invalid. Please refer to https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html"
+  validation {
+    condition     = length(regexall("^[a-z0-9][a-z0-9\\-.]{1,61}[a-z0-9]$", var.s3_bucket_name_replica)) > 0
+    error_message = "Input variable s3_bucket_name_replica is invalid. Please refer to https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html."
   }
 }
 
